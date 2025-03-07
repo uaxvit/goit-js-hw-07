e425d287bf26a186a75c098789429b079c966052
uaxvit <uaxvit@gmail.com>
2025-03-07 22:33:47 +0200

    HW 7

--- js/task-3.js
+++ js/task-3.js
@@ -1,27 +1,6 @@
-class StringBuilder {
-  #value;
-  constructor(initialValue) {
-    this.#value = initialValue;
-  }
-  getValue() {
-    return this.#value;
-  }
-  padEnd(str) {
-    this.#value += str;
-  }
-  padStart(str) {
-    this.#value = str + this.#value;
-  }
-  padBoth(str) {
-    this.#value = str + this.#value + str;
-  }
-}
-
-const builder = new StringBuilder(".");
-console.log(builder.getValue()); // "."
-builder.padStart("^");
-console.log(builder.getValue()); // "^."
-builder.padEnd("^");
-console.log(builder.getValue()); // "^.^"
-builder.padBoth("=");
-console.log(builder.getValue()); // "=^.^="
+const nameInput = document.querySelector('#name-input');
+const nameOutput = document.querySelector('#name-output');
+nameInput.addEventListener('input', event => {
+  const trimmedValue = event.target.value.trim();
+  nameOutput.textContent = trimmedValue === '' ? 'Anonymous' : trimmedValue;
+});
